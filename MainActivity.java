14e927ba389f0653f525a20b10c5ecffc7de5fc3
sayani13 <37878386+sayani13@users.noreply.github.com>
2018-03-28 22:50:10 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.student.hellogridview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridView;
+        gridView = findViewById(R.id.gridView);
+        gridView.setAdapter(new ImageAdapter(this));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,""+position,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+}
